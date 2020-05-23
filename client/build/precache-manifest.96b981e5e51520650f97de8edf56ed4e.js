97b6b26e39410058c232ebf9bf7d7eb85ce2c188
David Borde <dborde@gogoair.com>
2020-05-23 15:31:37 -0500

    feat: adding build

---
+++ client/build/precache-manifest.96b981e5e51520650f97de8edf56ed4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4a36dc6c8ab02076505c93e2352b85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e201683e9796ad68589",
+    "url": "/static/css/main.577e914d.chunk.css"
+  },
+  {
+    "revision": "c069afc09eb9bbaabb88",
+    "url": "/static/js/2.4251ecf1.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.4251ecf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e201683e9796ad68589",
+    "url": "/static/js/main.432fee1a.chunk.js"
+  },
+  {
+    "revision": "a0ffbd719b581b5728e0",
+    "url": "/static/js/runtime-main.d9906784.js"
+  }
+]);
