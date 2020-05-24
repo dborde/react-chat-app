027aa85e9d1d04c17f99a9e0b169179be7d3f187
David Borde <dborde@gogoair.com>
2020-05-24 01:07:26 -0500

    Adding `build` to source control

---
+++ client/build/precache-manifest.bb56532d902b885f288dc8c85b99029c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1e8fec1ed566ff35b7b6b0fb3cf3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1574e6d22561acd946cc",
+    "url": "/static/css/main.577e914d.chunk.css"
+  },
+  {
+    "revision": "c069afc09eb9bbaabb88",
+    "url": "/static/js/2.4251ecf1.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.4251ecf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1574e6d22561acd946cc",
+    "url": "/static/js/main.4445e2e3.chunk.js"
+  },
+  {
+    "revision": "a0ffbd719b581b5728e0",
+    "url": "/static/js/runtime-main.d9906784.js"
+  }
+]);
