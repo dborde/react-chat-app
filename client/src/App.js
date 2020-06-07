f7cdda2fce14f3affed2ec9d3036cbf01e44f447
David Borde <dborde@gogoair.com>
2020-06-07 17:18:20 -0500

    adding context provider and touch swipe

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import classNames from 'classnames';
-import Join from './pages/join';
-import Chat from './pages/chat';
+import classNames from "classnames";
+import Join from "./pages/join";
+import Chat from "./pages/chat";
 import "../src/assets/styles/_grid.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      site_loaded: false
-    }
+      site_loaded: false,
+    };
   }
 
   componentDidMount() {
     this.setState({
-      site_loaded: true
+      site_loaded: true,
     });
   }
 
   render() {
     return (
-      <div className={classNames({'App': true, 'site_loaded': this.state.site_loaded})}>
+      <div
+        className={classNames({
+          App: true,
+          site_loaded: this.state.site_loaded,
+        })}
+      >
         <Router>
-            <Switch>
-              <Route path="/chat/:username/:room" component={Chat} />
-              <Route path="/" component={Join} />
-            </Switch>
-          </Router>
+          <Switch>
+            <Route path="/chat/:username/:room" component={Chat} />
+            <Route path="/" component={Join} />
+          </Switch>
+        </Router>
       </div>
     );
   }
--- client/src/pages/join.js
+++ client/src/pages/join.js
@@ -1,53 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router-dom";
 import io from "socket.io-client";
 import { Rooms } from "../components/Rooms";
 
-const ENDPOINT = process.env.NODE_ENV === 'production' ? "https://borde-react-chat-app.herokuapp.com/" : "http://localhost:5000";
+const ENDPOINT =
+  process.env.NODE_ENV === "production"
+    ? "https://borde-react-chat-app.herokuapp.com/"
+    : "http://localhost:5000";
 const socket = io(ENDPOINT);
 
 const Join = (props) => {
   const [rooms, setRooms] = useState([]);
-  const [values, setValues] = useState({room: '', username: ''});
+  const [values, setValues] = useState({ room: "", username: "" });
 
   useEffect(() => {
-    socket.on('roomsList', ( {rooms} ) => {
+    socket.on("roomsList", ({ rooms }) => {
       setRooms(rooms);
     });
-    console.log('rooms', rooms);
   }, [rooms]);
 
   useEffect(() => {
-    return () => socket.off('roomsList');   
+    return () => socket.off("roomsList");
   }, []);
 
-  const onInputUpdate = e => {
-    const {name, value} = e.target;
-    setValues({...values, [name]: value});
-  }
+  const onInputUpdate = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
   const join = () => {
     const { username, room } = values;
     if (username && room) {
       props.history.push(`/chat/${username}/${room}`);
     }
-  }
+  };
 
   return (
     <div className="row">
       <div className="col-xs">
         <form className="centered-form__box">
           <h2>Join</h2>
-          <input name="username" placeholder="Display name" autoComplete="off" value={values.username} onChange={onInputUpdate} />
-          <input type="text" list="rooms" name="room" placeholder="Room" autoComplete="off" value={values.room} onChange={onInputUpdate} />
+          <input
+            name="username"
+            placeholder="Display name"
+            autoComplete="off"
+            value={values.username}
+            onChange={onInputUpdate}
+          />
+          <input
+            type="text"
+            list="rooms"
+            name="room"
+            placeholder="Room"
+            autoComplete="off"
+            value={values.room}
+            onChange={onInputUpdate}
+          />
           <datalist id="rooms">
             <Rooms rooms={rooms} />
           </datalist>
-          <button onClick={join}>Join</button>
+          <button onClick={join} type="button">
+            Join
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default withRouter(Join);
